f49e8de5b148ac40a63e48e8b810bb3c87d873ba
tomoaki_narukawa <tomo111911111@gmail.com>
2022-12-05 00:50:47 +0900

    agendas_controllerにdestroyアクションを追加

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -15,9 +15,17 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
+    end
+  end
+
+  def destroy
+    set_agenda
+    if current_user.id == @agenda.user.id || current_user.id == @agenda.team.owner_id
+      @agenda.destroy
+      redirect_to dashboard_url
     end
   end
 
